8bb49812489ca438e3962d6ad250059345cadccd
wangjontao <54728149+wangjontao@users.noreply.github.com>
2026-09-20 21:12:55 +0800

    Fix G33Pro deferred DTS and image size expansion

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -1262,11 +1262,11 @@ define Device/gielink_g33pro-v1
   IMAGE_SIZE := 113152k
   KERNEL_IN_UBI := 1
   IMAGES += factory.bin
-  IMAGE/factory.bin := append-ubi | check-size $(IMAGE_SIZE)
+  IMAGE/factory.bin := append-ubi | check-size $$(IMAGE_SIZE)
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
-  KERNEL := kernel-bin | lzma | \
+  KERNEL = kernel-bin | lzma | \
 	fit lzma $(KDIR)/image-$(firstword $(DEVICE_DTS)).dtb
-  KERNEL_INITRAMFS := kernel-bin | lzma | \
+  KERNEL_INITRAMFS = kernel-bin | lzma | \
 	fit lzma $(KDIR)/image-$(firstword $(DEVICE_DTS)).dtb with-initrd
 endef
 TARGET_DEVICES += gielink_g33pro-v1
